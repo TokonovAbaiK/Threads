c567533a5990da751cb5bd8200a8d1f292d5b974
Omuremes01~ <megamansous@gmail.com>
2023-10-26 23:23:37 +0600

    topbar and sidebar so close finished

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,26 @@
+import Link from "next/link";
+import {sidebarLinks} from '@/constants/index'
+import Image from "next/image";
 function LeftSidebar(){
     return (
-        <h1>LeftSidebar</h1>
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => (
+                    <Link 
+                        href={link.route}
+                        className="leftsidebar_link"
+                        key={link.label}
+                    >
+                       <Image
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                       />
+                    </Link>
+                ))}
+            </div>
+        </section>
     )
 }
 
